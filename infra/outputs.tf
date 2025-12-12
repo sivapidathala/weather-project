166fdde6bf3f4328b6f3b0035a80b9cd2929c808
sivapidathala <sivapidathala123@gmail.com>
2025-12-12 08:36:06 +0000

    first commit

---
+++ infra/outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_name" {
+  value = aws_s3_bucket.weather_bucket.bucket
+}
+
+output "public_bucket_url" {
+  value = "https://${aws_s3_bucket.weather_bucket.bucket}.s3.${var.aws_region}.amazonaws.com/"
+}
